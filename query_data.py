80be1c056ee040820185338a5ab8000307870532
Evan Naderi <evan.naderi@gmail.com>
2023-10-07 23:03:21 -0400

    added seperate functions for prompting, processing, and loading

--- query_data.py
+++ query_data.py
@@ -1,4 +1,3 @@
-# PDF Loaders. If unstructured gives you a hard time, try PyPDFLoader
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
@@ -8,26 +7,37 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
-loader = UnstructuredPDFLoader('documents/AFA10.pdf')
 api_key = os.getenv("OPENAI_API_KEY")
 
-data = loader.load()
-print(f'You have {len(data)} documents in your data')
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=50)
-texts = text_splitter.split_documents(data)
+def load_and_convert_pdf(pdf_path):
+    loader = UnstructuredPDFLoader(pdf_path)
+    data = loader.load()
+    print(f'You have {len(data)} documents in your data')
+    return data
 
-embeddings_model = OpenAIEmbeddings(openai_api_key=api_key)
-doc_search = Chroma.from_documents(texts, embeddings_model)
+def process_text_and_get_embeddings(data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=50)
+    texts = text_splitter.split_documents(data)
+    embeddings_model = OpenAIEmbeddings(openai_api_key=api_key)
+    doc_search = Chroma.from_documents(texts, embeddings_model)
+    print(f'You now have {len(texts)} texts in your data')
+    return doc_search
 
-print(f'You now have {len(texts)} texts in your data')
+def send_prompt_and_get_response(doc_search, query):
+    llm = OpenAI(temperature=0, openai_api_key=api_key)
+    chain = load_qa_chain(llm, chain_type="stuff")
+    docs = doc_search.similarity_search(query)
+    res = chain.run(input_documents=docs, question=query)
+    print(res)
+    return res
 
-llm = OpenAI(temperature=0, openai_api_key=api_key)
-chain = load_qa_chain(llm, chain_type="stuff")
-
-# replace with your own query
+# Replace 'documents/AFA10.pdf' and 'Which is not covered under bodily injury?' with your own values
+pdf_path = 'documents/AFA10.pdf'
 query = "Which is not covered under bodily injury?"
-docs = doc_search.similarity_search(query)
 
-res = chain.run(input_documents=docs, question=query)
-print(res)
+# Now you can call these functions in sequence to carry out the process.
+data = load_and_convert_pdf(pdf_path)
+doc_search = process_text_and_get_embeddings(data)
+response = send_prompt_and_get_response(doc_search, query)
