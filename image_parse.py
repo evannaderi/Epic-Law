beb52300acf6ab4495939b14dc4077e07f761ef3
Evan Naderi <evan.naderi@gmail.com>
2023-10-07 21:12:50 -0400

    image parse working

---
+++ image_parse.py
@@ -0,0 +1,25 @@
+from google.cloud import vision_v1
+from google.oauth2 import service_account
+import io
+
+def extract_text_from_image_pdf(pdf_path, output_path):
+    creds = service_account.Credentials.from_service_account_file(
+        'intrepid-alloy-401317-c6027ebba5eb.json')
+    client = vision_v1.ImageAnnotatorClient(credentials=creds)
+
+    with io.open(pdf_path, 'rb') as pdf_file:
+        content = pdf_file.read()
+
+    image = vision_v1.Image(content=content)
+    response = client.document_text_detection(image=image)
+
+    all_text = response.full_text_annotation.text
+
+    with open(output_path, 'w', encoding="utf-8") as output_file:
+        output_file.write(all_text)
+
+if __name__ == "__main__":
+    input_pdf_path = "documents/test.jpeg"
+    output_txt_path = "output.txt"
+
+    extract_text_from_image_pdf(input_pdf_path, output_txt_path)
